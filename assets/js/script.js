3a538e75e9d8fbcc1f0aed0208af997c20e73691
sohailshams <sohailshamas@yahoo.com>
2021-02-19 11:49:57 +0100

    Created a function to decide the winner and added a game data object

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ function rpsGame(yourChoice) {
   let userChoice, pcChoice;
   userChoice = yourChoice.id;
   pcChoice = numberToChoice(randomToIntiger());
+  let result = winner(userChoice, pcChoice);
 }
 
 function randomToIntiger() {
@@ -10,4 +11,27 @@ function randomToIntiger() {
 
 function numberToChoice(number) {
   return ['rock', 'paper', 'scissor'][number];
+}
+
+function winner(userChoice, pcChoice) {
+  var gameData = {
+    rock: {
+      scissor: 1,
+      rock: 0.5,
+      paper: 0,
+    },
+    paper: {
+      rock: 1,
+      paper: 0.5,
+      scissor: 0,
+    },
+    scissor: {
+      paper: 1,
+      scissor: 0.5,
+      rock: 0,
+    },
+  };
+  let userScore = gameData[userChoice][pcChoice];
+  let pcScore = gameData[pcChoice][userChoice];
+  return [userScore, pcScore];
 }
